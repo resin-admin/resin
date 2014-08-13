aea183dd7a2ad9e0b0204116d93081daacdcb0aa
themarkappleby <themarkappleby@gmail.com>
2014-08-13 16:26:29 -0400

    updated Guidedog and added documentation for the form and tab components

--- assets/js/resin.js
+++ assets/js/resin.js
@@ -32,6 +32,6 @@ $(document).ready(function(){
 
 // http://stackoverflow.com/a/13542669/918060
 function shadeRGBColor(color, percent) {
-  var f=color.split(","),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=parseInt(f[0].slice(4)),G=parseInt(f[1]),B=parseInt(f[2]);
-  return "rgb("+(Math.round((t-R)*p)+R)+","+(Math.round((t-G)*p)+G)+","+(Math.round((t-B)*p)+B)+")";
+  //var f=color.split(","),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=parseInt(f[0].slice(4)),G=parseInt(f[1]),B=parseInt(f[2]);
+  //return "rgb("+(Math.round((t-R)*p)+R)+","+(Math.round((t-G)*p)+G)+","+(Math.round((t-B)*p)+B)+")";
 }
