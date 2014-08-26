2631086d35e1e5ef16e435a6023189b6b35d4f7f
themarkappleby <themarkappleby@gmail.com>
2014-08-26 11:23:31 -0400

    re-worked server setup

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
 
+var path = require('path')
 var connect = require('connect')
 var serveStatic = require('serve-static')
 
-var port = process.env.PORT || 8080
-connect().use(serveStatic(__dirname)).listen(port)
+var port = process.env.PORT || 3000
+var server = connect()
+
+server.use('/vendor', serveStatic(path.join(__dirname, '/vendor')))
+server.use(serveStatic(__dirname+'/dist'))
+server.listen(port)
 
 console.log('Listening on port', port)
